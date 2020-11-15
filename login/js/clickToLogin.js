8c195b3f1c6797535b762cf3e21736ba794672bc
Hziee-maoyuhuan <1163835572@qq.com>
2020-11-15 14:08:07 +0800

    some changes

---
+++ login/js/clickToLogin.js
@@ -0,0 +1,16 @@
+// $({function (){
+//         let username = $("#username").text;
+//         let userpassword = $("#userpassword").text;
+//
+//         alert(username)
+//         alert(userpassword)
+//     }
+// })
+
+$(".loginButton").click(function () {
+    let username = $("#username").text;
+    let userpassword = $("#userpassword").text;
+
+    alert(username)
+    alert(userpassword)
+});
