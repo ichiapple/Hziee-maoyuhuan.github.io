a9dbbbcc7242b8cc324600b276b011209c20cb7d
Hziee-maoyuhuan <1163835572@qq.com>
2020-11-15 23:20:26 +0800

    fixed login partition

--- login/js/clickToLogin.js
+++ login/js/clickToLogin.js
@@ -1,19 +1,21 @@
-function check(){
-    var name=document.username;
-    var password=document.userpassword;
-    if(name.value===""){
-        alert("用户名不能为空");
-        name.focus();
-        return false;
-    }
-    if(password.value===""){
-        alert("密码不能为空")
-        password.focus();
-        return false;
-    }
+function check() {
+    var name = $(".username").val()
+    var password = $(".userpassword").val()
 
-    if ("admin"===name&&"root"===password){
+    alert("name" + name)
+    alert("password" + password)
+
+    if (name === "") {
+        alert("用户名不能为空");
+        $(".username").focus();
+    } else if (password === "") {
+        alert("密码不能为空")
+        $(".userpassword").focus();
+    } else if ("admin" === name && "root" === password) {
+        alert("登陆成功")
         window.location = "https://hziee-maoyuhuan.github.io/index/index.html";
+    } else {
+        alert("用户名或密码错误")
+        location.reload();
     }
-    location.reload();
 }
