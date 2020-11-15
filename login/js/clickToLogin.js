b98f69f2daa8e2b239a86b32e72c36eee578d93a
Hziee-maoyuhuan <1163835572@qq.com>
2020-11-15 22:33:30 +0800

    add login partition

--- login/js/clickToLogin.js
+++ login/js/clickToLogin.js
@@ -1,16 +1,19 @@
-// $({function (){
-//         let username = $("#username").text;
-//         let userpassword = $("#userpassword").text;
-//
-//         alert(username)
-//         alert(userpassword)
-//     }
-// })
+function check(){
+    var name=document.username;
+    var password=document.userpassword;
+    if(name.value===""){
+        alert("用户名不能为空");
+        name.focus();
+        return false;
+    }
+    if(password.value===""){
+        alert("密码不能为空")
+        password.focus();
+        return false;
+    }
 
-$(".loginButton").click(function () {
-    let username = $("#username").text;
-    let userpassword = $("#userpassword").text;
-
-    alert(username)
-    alert(userpassword)
-});
+    if ("admin"===name&&"root"===password){
+        window.location = "https://hziee-maoyuhuan.github.io/index/index.html";
+    }
+    location.reload();
+}
