e2663c7c68d6f2a72eade738f9c538d3bd9f6cb9
Hziee-maoyuhuan <1163835572@qq.com>
2020-11-20 20:48:11 +0800

    changed

---
+++ learning/vue/163-vue-music/src/components/test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test",
+  data() {
+    return {
+      msg: "大家好 我是test.vue",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
