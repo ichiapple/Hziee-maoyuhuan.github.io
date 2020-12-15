bcd3d986c258ded6d2938ee08abd84c792c00b0a
Hziee-maoyuhuan <1163835572@qq.com>
2020-12-15 13:48:00 +0800

    update

---
+++ learning/javaee/springBoot/demo/src/test/java/cn/hziee/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package cn.hziee.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+@RestController
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
