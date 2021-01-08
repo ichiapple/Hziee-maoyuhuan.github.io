2af35f2e7d27fe938515a5eaafcdf4f5fdfc2759
Hziee-maoyuhuan <1163835572@qq.com>
2021-01-08 13:55:52 +0800

    update files

--- index_page/js/index.js
+++ index_page/js/index.js
@@ -2,6 +2,7 @@
 const wordEle = document.querySelector('.word');
 // 声明变量
 let word = "Hello World.";
+let word2 = "I am Mao Yuhuan.";
 
 // 使用async和await实现
 // 封装一个函数
@@ -19,6 +20,10 @@ function setText(text) { //为标签元素设置文本
 async function main() {
     for (let i = 0; i <= word.length; i++) {
         await setText(word.substr(0, i))
+    }
+
+    for (let i = 0; i <= word2.length; i++) {
+        await setText(word + word2.substr(0, i))
     }
 }
 main();
