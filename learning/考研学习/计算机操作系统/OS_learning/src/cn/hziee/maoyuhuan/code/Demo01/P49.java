c3e38c2720c2bd643f8b77750ade63c6b79f769d
Hziee-maoyuhuan <1163835572@qq.com>
2021-01-22 16:37:46 +0800

    udpate 计算机操作系统

---
+++ learning/考研学习/计算机操作系统/OS_learning/src/cn/hziee/maoyuhuan/code/Demo01/P49.java
@@ -0,0 +1,20 @@
+package cn.hziee.maoyuhuan.code.Demo01;
+
+/**
+ * Created By fct on 2021/1/22
+ * Using SoftWare  :   IntelliJ IDEA
+ * ProjectName     :   OS_learning
+ */
+public class P49 {
+	int in = 0, out = 0, count = 0;
+	item buffer[n];
+
+	void producer() {
+		while (1):{
+			produce an item in netp;
+			...
+			while (counter == n) ;
+			buffer[i]
+		}
+	}
+}
